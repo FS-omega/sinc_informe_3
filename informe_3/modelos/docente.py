2511626ddaabe85cb9fd518b6c9fc8454ef15a6e
fcu2134 <facundo.marin.moscol@cftmail.cl>
2023-11-28 20:20:42 -0300

    FC2

---
+++ informe_3/modelos/docente.py
@@ -0,0 +1,6 @@
+class docente:
+    def __init__(self,id_docente ,d_nombre,email,jornada_id):
+        self.id_docente = id_docente
+        self.d_nombre = d_nombre
+        self.email = email
+        self.jornada_id = jornada_id
