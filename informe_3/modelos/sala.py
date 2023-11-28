5d85169bf4f42b48edceb6267120f79b128761fb
fcu2134 <facundo.marin.moscol@cftmail.cl>
2023-11-28 20:03:05 -0300

    FC 1

---
+++ informe_3/modelos/sala.py
@@ -0,0 +1,7 @@
+class sala:
+    def __init__(self,sala_id,capacidad_sala,sala_tipo,id_usuario,estado_id ):
+        self.sala_id = sala_id,
+        self.capacidad_sala = capacidad_sala,
+        self.sala_tipo = sala_tipo,
+        self.id_usuario = id_usuario
+        self.estado_id = estado_id
