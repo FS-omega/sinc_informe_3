c9736e95277587e9efed370a9355a5c31055ca81
fcu2134 <facundo.marin.moscol@cftmail.cl>
2023-11-28 20:23:35 -0300

    FC4

---
+++ informe_3/modelos/carr_mod.py
@@ -0,0 +1,5 @@
+class carr_mod:
+    def __init__(self,carre_mod_id,modulo_id ,id_carrera):
+        self.carre_mod_id_= carre_mod_id
+        self.modulo_id= modulo_id
+        self.id_carrera = id_carrera
