b6b89ddaddc1d775e0b32e8fcd43b209a7610263
user.name like REIMOC96 <reinaldo.marcelo.ordonez@cftmail.cl>
2023-11-28 22:18:53 -0300

    estado.py arreglado

--- informe_3/modelos/estado.py
+++ informe_3/modelos/estado.py
@@ -1,3 +1,6 @@
 class estado:
-    def __init__(self,id_estado):
-     self.id_estado = id_estado
+    def __init__(self,id_estadoid_usuario,descripcion,fecha_hora):
+        self.estado_id = estado_id
+        self.id_usuario = id_usuario
+        self.descripcion = escripcion
+        self.fecha_hora = fecha_hora
