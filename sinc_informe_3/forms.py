c86978139a99cbe1da6dfaa6b3d1857620d29155
user.name like REIMOC96 <reinaldo.marcelo.ordonez@cftmail.cl>
2023-12-01 04:37:34 -0300

    ª

--- sinc_informe_3/forms.py
+++ sinc_informe_3/forms.py
@@ -1,7 +1,2 @@
 from django import forms
-from .models import Docente
-
-class DocenteForm(forms.ModelForm):
-    class Meta:
-        model = Docente
-        fields = ['id_docente', 'email', 'jornada_id', 'd_nombre']
+from models import * 
